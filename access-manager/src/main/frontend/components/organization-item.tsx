450d495aa3f9d6d88bc595767d37ec65368f343e
Dennis Schäfer <dennis.schaefer94@gmail.com>
2024-07-19 16:29:23 +0200

    Implemented dialog to create an organization in the frontend

--- access-manager/src/main/frontend/components/organization-item.tsx
+++ access-manager/src/main/frontend/components/organization-item.tsx
@@ -1,13 +1,14 @@
 import React, {CSSProperties} from 'react';
 import type Organization from "Frontend/generated/rize/os/access/manager/organization/Organization.js";
-import { MenuBar } from '@vaadin/react-components/MenuBar.js';
+import {MenuBar, MenuBarItemSelectedEvent} from '@vaadin/react-components/MenuBar.js';
 import Apartment from '@mui/icons-material/Apartment';
 import { MoreVert, Edit, Delete } from "@mui/icons-material";
 
 interface OrganizationItemProps {
     organization: Organization,
     className?: string,
-    style?: CSSProperties
+    style?: CSSProperties,
+    onEdit?: (organization: Organization) => void
 }
 
 const separatorStyle = {
@@ -20,14 +21,20 @@ const menuBarItems = [
         className: "text-gray-500 rounded-full p-0 m-0",
         theme: "icon tertiary",
         children: [
-            { component: <><Edit style={{transform: "scale(0.8)"}}/><p className={"mx-2"}>Edit</p></> },
-            { component: <><Delete style={{transform: "scale(0.8)"}}/><p className={"mx-2"}>Delete</p></>
-            }
+            { component: <><Edit style={{transform: "scale(0.8)"}}/><p className={"mx-2"}>Edit</p></>, key: "edit"},
+            { component: <><Delete style={{transform: "scale(0.8)"}}/><p className={"mx-2"}>Delete</p></> , key: "delete"}
         ]
     }
 ]
 
-const OrganizationItem: React.FC<OrganizationItemProps> = ({ organization , className, style}) => {
+const OrganizationItem: React.FC<OrganizationItemProps> = ({ organization , className, style, onEdit}) => {
+
+    const handleMenuItemSelected = (e: MenuBarItemSelectedEvent) => {
+        // @ts-ignore
+        if (e.detail.value.key === "edit")
+            onEdit?.(organization);
+    }
+
     return (
         <div className={"border border-contrast-10 rounded-xl flex flex-col box-border " + className} style={style}>
             <section className={"flex flex-row gap-3.5 justify-center p-4 items-center"}>
@@ -41,7 +48,7 @@ const OrganizationItem: React.FC<OrganizationItemProps> = ({ organization , clas
                     <span className={"text-sm text-gray-400 leading-5"}>{organization.alias}</span>
                 </div>
                 <div>
-                    <MenuBar theme="icon" items={menuBarItems} onItemSelected={e => console.log(e)}/>
+                    <MenuBar theme="icon" items={menuBarItems} onItemSelected={handleMenuItemSelected}/>
                 </div>
             </section>
 
--- access-manager/src/main/java/rize/os/access/manager/organization/OrganizationEndpoint.java
+++ access-manager/src/main/java/rize/os/access/manager/organization/OrganizationEndpoint.java
@@ -2,6 +2,7 @@ package rize.os.access.manager.organization;
 
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.hilla.Endpoint;
+import jakarta.annotation.Nonnull;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class OrganizationEndpoint
      * Returns all organizations
      * @return List of organizations
      */
+    @Nonnull
     public List<Organization> findAll()
     {
         return organizationService.findAll();
@@ -27,8 +29,44 @@ public class OrganizationEndpoint
      * @param search Search term to filter organizations
      * @return List of organizations that matches the search term
      */
-    public List<Organization> find(String search)
+    @Nonnull
+    public List<Organization> find(@Nonnull String search)
     {
         return organizationService.find(search);
+    }
+
+    /**
+     * Determines wether an organization with the given name already exists.
+     * @param name Name of the organization to check
+     * @return {@code true} if an organization with the given name exists, {@code false} otherwise
+     */
+    public boolean nameExists(@Nonnull String name)
+    {
+        return organizationService.find(name)
+                .stream().anyMatch(organization -> organization.getName().equalsIgnoreCase(name));
+    }
+
+    /**
+     * Determines wether an organization with the given alias already exists.
+     * @param alias Alias of the organization to check
+     * @return {@code true} if an organization with the given alias exists, {@code false} otherwise
+     */
+    public boolean aliasExists(@Nonnull String alias)
+    {
+        return organizationService.findAll()
+                .stream().anyMatch(organization -> organization.getAlias().equalsIgnoreCase(alias));
+    }
+
+    /**
+     * Creates a new organization with the given name and alias.
+     * @param name Name of the organization
+     * @param alias Alias of the organization
+     * @return The created organization
+     */
+    @Nonnull
+    public Organization create(@Nonnull String name, @Nonnull String alias)
+    {
+        var organization = Organization.builder().name(name).alias(alias).build();
+        return organizationService.createOrganization(organization);
     }
 }
