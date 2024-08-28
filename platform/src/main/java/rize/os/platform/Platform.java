74b3cb2df98ffc7a1d04650d1f379f0bce60fe3e
Dennis Schäfer <dennis.schaefer94@gmail.com>
2024-08-28 13:12:55 +0200

    Added settings to build native image of platform

--- platform/src/main/java/rize/os/platform/Platform.java
+++ platform/src/main/java/rize/os/platform/Platform.java
@@ -1,12 +1,15 @@
 package rize.os.platform;
 
+import com.vaadin.hilla.springnative.HillaHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @Theme("rize-os")
 @SpringBootApplication
+@ImportRuntimeHints({ PlatformHintsRegistrar.class, HillaHintsRegistrar.class })
 public class Platform implements AppShellConfigurator
 {
     public static void main(String[] args)
