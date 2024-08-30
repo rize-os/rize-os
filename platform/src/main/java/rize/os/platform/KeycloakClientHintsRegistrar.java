8eed2499527ace7b8142ba94ea8a26209e0b767c
Dennis Schäfer <dennis.schaefer94@gmail.com>
2024-08-30 11:36:51 +0200

    Added runtime hints for KeycloakClient

--- platform/src/main/java/rize/os/platform/KeycloakClientHintsRegistrar.java
+++ platform/src/main/java/rize/os/platform/KeycloakClientHintsRegistrar.java
@@ -2,7 +2,6 @@ package rize.os.platform;
 
 import org.keycloak.admin.client.spi.ResteasyClientClassicProvider;
 import org.springframework.aot.hint.ExecutableMode;
-import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.lang.NonNull;
