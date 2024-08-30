0bdac1ba66502ed0663cb13e3c0b72e6c2330c4c
Dennis Schäfer <dennis.schaefer94@gmail.com>
2024-08-30 11:36:25 +0200

    Added runtime hints for KeycloakClient

--- platform/src/main/java/rize/os/platform/KeycloakClientHintsRegistrar.java
+++ platform/src/main/java/rize/os/platform/KeycloakClientHintsRegistrar.java
@@ -1,10 +1,13 @@
 package rize.os.platform;
 
 import org.keycloak.admin.client.spi.ResteasyClientClassicProvider;
+import org.springframework.aot.hint.ExecutableMode;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.lang.NonNull;
+
+import java.util.Arrays;
 
 public class KeycloakClientHintsRegistrar implements RuntimeHintsRegistrar
 {
@@ -16,6 +19,7 @@ public class KeycloakClientHintsRegistrar implements RuntimeHintsRegistrar
 
     private void registerClass(Class<?> clazz, RuntimeHints hints)
     {
-        hints.reflection().registerType(clazz, MemberCategory.values());
+        hints.reflection().registerType(clazz);
+        Arrays.stream(clazz.getMethods()).toList().forEach(method -> hints.reflection().registerMethod(method, ExecutableMode.INVOKE));
     }
 }
