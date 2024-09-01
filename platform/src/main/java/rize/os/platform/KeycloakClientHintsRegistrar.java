d97be9065f7a2eafbed3d81d05ccbd41b3f49e24
Dennis Schäfer <dennis.schaefer@rafi-group.com>
2024-09-01 18:21:08 +0200

    Modified Runtime Hints

--- platform/src/main/java/rize/os/platform/KeycloakClientHintsRegistrar.java
+++ platform/src/main/java/rize/os/platform/KeycloakClientHintsRegistrar.java
@@ -1,6 +1,7 @@
 package rize.os.platform;
 
 import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
+import org.jboss.resteasy.plugins.providers.jaxb.i18n.LogMessages;
 import org.jboss.resteasy.resteasy_jaxrs.i18n.Messages_$bundle;
 import org.keycloak.admin.client.spi.ResteasyClientClassicProvider;
 import org.springframework.aot.hint.ExecutableMode;
@@ -15,14 +16,26 @@ public class KeycloakClientHintsRegistrar implements RuntimeHintsRegistrar
     @Override
     public void registerHints(@NonNull RuntimeHints hints, ClassLoader classLoader)
     {
-        registerClass(ResteasyClientClassicProvider.class, hints);
-        registerClass(ResteasyClientBuilderImpl.class, hints);
-        registerClass(Messages_$bundle.class, hints);
+        registerClass(hints, ResteasyClientClassicProvider.class);
+        registerClass(hints, ResteasyClientBuilderImpl.class);
+        registerClass(hints, LogMessages.class);
+        registerClass(hints, Messages_$bundle.class);
+        registerFields(hints, Messages_$bundle.class, "INSTANCE");
     }
 
-    private void registerClass(Class<?> clazz, RuntimeHints hints)
+    private void registerClass(RuntimeHints hints, Class<?> clazz)
     {
         Arrays.stream(clazz.getConstructors()).toList().forEach(constructor -> hints.reflection().registerConstructor(constructor, ExecutableMode.INVOKE));
         Arrays.stream(clazz.getMethods()).toList().forEach(method -> hints.reflection().registerMethod(method, ExecutableMode.INVOKE));
+    }
+
+    private void registerFields(RuntimeHints hints, Class<?> clazz, String... fields)
+    {
+        try
+        {
+            for (String field : fields)
+                hints.reflection().registerField(clazz.getField(field));
+        }
+        catch (Exception ignore) { }
     }
 }
