9416d5b0a22bad4e47c12c72559f62c8b8fbe58b
Dennis Schäfer <dennis.schaefer94@gmail.com>
2024-09-11 15:10:10 +0200

    Installed Tailwind CSS

---
+++ platform/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["src/main/frontend/index.html", "src/main/frontend/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
