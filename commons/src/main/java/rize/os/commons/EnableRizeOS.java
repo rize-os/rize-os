6895677e21ef9e1092cdf256b9c9160a3e00ad89
Dennis Schäfer <dennis.schaefer94@gmail.com>
2024-10-27 11:07:43 +0100

    Created Annotation to enable component scan for RizeOS

---
+++ commons/src/main/java/rize/os/commons/EnableRizeOS.java
@@ -0,0 +1,16 @@
+package rize.os.commons;
+
+import org.springframework.context.annotation.ComponentScan;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ComponentScan("rize.os")
+public @interface EnableRizeOS
+{
+    String value() default "";
+}
