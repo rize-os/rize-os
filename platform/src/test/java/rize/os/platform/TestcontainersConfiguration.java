cc2c012e89a55abdc426f87a02e3c1a198becc0d
Dennis Schäfer <dennis.schaefer94@gmail.com>
2024-11-02 13:19:19 +0100

    Added Pulsar to TestcontainersConfiguration

--- platform/src/test/java/rize/os/platform/TestcontainersConfiguration.java
+++ platform/src/test/java/rize/os/platform/TestcontainersConfiguration.java
@@ -3,11 +3,17 @@ package rize.os.platform;
 import dasniko.testcontainers.keycloak.KeycloakContainer;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.PulsarContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
 public class TestcontainersConfiguration
 {
+    private static final String KEYCLOAK_IMAGE = "quay.io/keycloak/keycloak:25.0.6";
+    private static final String POSTGRES_IMAGE = "postgres:17.0";
+    private static final String PULSAR_IMAGE = "apachepulsar/pulsar:4.0.0";
+
     private static final String KEYCLOAK_ADMIN_USERNAME = "admin";
     private static final String KEYCLOAK_ADMIN_PASSWORD = "secret";
 
@@ -17,21 +23,26 @@ public class TestcontainersConfiguration
 
     static KeycloakContainer keycloak;
     static PostgreSQLContainer<?> postgresql;
+    static PulsarContainer pulsar;
 
     static
     {
-        keycloak = new KeycloakContainer("quay.io/keycloak/keycloak:25.0.6")
+        keycloak = new KeycloakContainer(KEYCLOAK_IMAGE)
                 .withFeaturesEnabled("organization")
                 .withRealmImportFile("/realm.json")
                 .withAdminUsername(KEYCLOAK_ADMIN_USERNAME)
                 .withAdminPassword(KEYCLOAK_ADMIN_PASSWORD);
         keycloak.start();
 
-        postgresql = new PostgreSQLContainer("postgres:17.0")
+        postgresql = new PostgreSQLContainer(POSTGRES_IMAGE)
                 .withDatabaseName(POSTGRES_DATABASE_NAME)
                 .withUsername(POSTGRES_USERNAME)
                 .withPassword(POSTGRES_PASSWORD);
         postgresql.start();
+
+        pulsar = new PulsarContainer(DockerImageName.parse(PULSAR_IMAGE))
+                .withCommand("bin/pulsar standalone");
+        pulsar.start();
     }
 
     public static void updateContainerProperties(DynamicPropertyRegistry registry)
@@ -43,5 +54,11 @@ public class TestcontainersConfiguration
         registry.add("database.url", TestcontainersConfiguration.postgresql::getJdbcUrl);
         registry.add("database.username", () -> POSTGRES_USERNAME);
         registry.add("database.password", () -> POSTGRES_PASSWORD);
+
+        System.out.println("Pulsar URL: " + TestcontainersConfiguration.pulsar.getPulsarBrokerUrl());
+        System.out.println("Pulsar HTTP URL: " + TestcontainersConfiguration.pulsar.getHttpServiceUrl());
+
+        registry.add("spring.pulsar.client.service-url", TestcontainersConfiguration.pulsar::getPulsarBrokerUrl);
+        registry.add("spring.pulsar.admin.service-url", TestcontainersConfiguration.pulsar::getHttpServiceUrl);
     }
 }
