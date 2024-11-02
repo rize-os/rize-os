d1212dbaf15b555050b59ffc03419d9be17affb4
Dennis Schäfer <dennis.schaefer94@gmail.com>
2024-11-02 14:21:13 +0100

    Fixed issues in OrganizationStatePublisherIT

--- platform/src/test/java/rize/os/platform/organization/OrganizationStatePublisherIT.java
+++ platform/src/test/java/rize/os/platform/organization/OrganizationStatePublisherIT.java
@@ -53,14 +53,15 @@ class OrganizationStatePublisherIT
         var createdOrganization = organizationService.createOrganization(organizationToCreate);
         var createdOrganizationDto = organizationMapper.toOrganizationDto(createdOrganization);
 
-        Thread.sleep(100);
+        Thread.sleep(50);
 
         // Update
-        createdOrganization.setDisplayName("Organization State Publisher Updated");
-        var updatedOrganization = organizationService.updateOrganization(createdOrganization);
+        var organizationToUpdate = organizationMapper.toOrganization(createdOrganizationDto);
+        organizationToUpdate.setDisplayName("Organization State Publisher Updated");
+        var updatedOrganization = organizationService.updateOrganization(organizationToUpdate);
         var updatedOrganizationDto = organizationMapper.toOrganizationDto(updatedOrganization);
 
-        Thread.sleep(100);
+        Thread.sleep(50);
 
         // Delete
         organizationService.deleteOrganization(updatedOrganization.getId());
@@ -69,26 +70,32 @@ class OrganizationStatePublisherIT
                 .fromTopic(PulsarMessagingConfiguration.ORGANIZATION_STATE_TOPIC)
                 .withSchema(Schema.AVRO(OrganizationState.class))
                 .awaitAtMost(Duration.ofSeconds(5))
-                .until(msg -> msg.size() == 3)
                 .get();
 
-        assertThat(messages).hasSize(3);
-        //var createMessage = messages.stream().filter(msg -> msg.getValue().getEventType() == OrganizationState.EventType.CREATED).findFirst().orElseThrow();
-        var createMessage = messages.getFirst();
-        assertThat(createMessage.getKey()).isEqualTo(createdOrganization.getId());
+        assertThat(messages.size()).isGreaterThanOrEqualTo(3);
+
+        var createMessage = messages.stream()
+                .filter(msg -> msg.getValue().getEventType() == OrganizationState.EventType.CREATED)
+                .filter(msg -> msg.getKey().equals(createdOrganization.getId()))
+                .findFirst().orElseThrow();
         assertThat(createMessage.getValue().getPayload().getBefore()).isNull();
         assertThat(createMessage.getValue().getPayload().getAfter()).isEqualTo(createdOrganizationDto);
 
-//        var updateMessage = messages.stream().filter(msg -> msg.getValue().getEventType() == OrganizationState.EventType.UPDATED).findFirst().orElseThrow();
-        var updateMessage = messages.get(1);
-        assertThat(updateMessage.getKey()).isEqualTo(updatedOrganization.getId());
+        var updateMessage = messages.stream()
+                .filter(msg -> msg.getValue().getEventType() == OrganizationState.EventType.UPDATED)
+                .filter(msg -> msg.getKey().equals(createdOrganization.getId()))
+                .findFirst().orElseThrow();
         assertThat(updateMessage.getValue().getPayload().getBefore()).isEqualTo(createdOrganizationDto);
         assertThat(updateMessage.getValue().getPayload().getAfter()).isEqualTo(updatedOrganizationDto);
 
-//        var deleteMessage = messages.stream().filter(msg -> msg.getValue().getEventType() == OrganizationState.EventType.DELETED).findFirst().orElseThrow();
-        var deleteMessage = messages.get(2);
-        assertThat(deleteMessage.getKey()).isEqualTo(updatedOrganization.getId());
+        var deleteMessage = messages.stream()
+                .filter(msg -> msg.getValue().getEventType() == OrganizationState.EventType.DELETED)
+                .filter(msg -> msg.getKey().equals(createdOrganization.getId()))
+                .findFirst().orElseThrow();
         assertThat(deleteMessage.getValue().getPayload().getBefore()).isEqualTo(updatedOrganizationDto);
         assertThat(deleteMessage.getValue().getPayload().getAfter()).isNull();
+
+        assertThat(messages.indexOf(createMessage)).isLessThan(messages.indexOf(updateMessage));
+        assertThat(messages.indexOf(updateMessage)).isLessThan(messages.indexOf(deleteMessage));
     }
 }
