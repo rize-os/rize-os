e7de2dc02a57ca74d3da34ecc2992feaa8ad1f94
Dennis Schäfer <dennis.schaefer94@gmail.com>
2025-07-23 23:41:06 +0200

    Created rize-cockpit module with Vaadin components

---
+++ rize-cockpit/src/main/frontend/views/@index.tsx
@@ -0,0 +1,9 @@
+export default function HomeView()
+{
+    return (
+        <div>
+            <h1>rize.cockpit</h1>
+        </div>
+  );
+}
+
---
+++ rize-cockpit/src/test/java/rize/os/cockpit/TestcontainersConfiguration.java
@@ -0,0 +1,28 @@
+package rize.os.cockpit;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.PulsarContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration
+{
+    /*
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer()
+    {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+
+    @Bean
+    @ServiceConnection
+    PulsarContainer pulsarContainer()
+    {
+        return new PulsarContainer(DockerImageName.parse("apachepulsar/pulsar:latest"));
+    }
+    */
+}
