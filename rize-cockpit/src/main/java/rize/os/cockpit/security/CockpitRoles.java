5f9c33c528b21adf773dc8928bb151ff5ccd745d
Dennis Schäfer <dennis.schaefer94@gmail.com>
2025-07-29 23:17:11 +0200

    Added required role "platform-administrator" to security configuration of rize.cockpit

---
+++ rize-cockpit/src/main/java/rize/os/cockpit/security/CockpitRoles.java
@@ -0,0 +1,8 @@
+package rize.os.cockpit.security;
+
+public class CockpitRoles
+{
+    public static final String PLATFORM_ADMIN = "platform-administrator";
+
+    private CockpitRoles() {}
+}
---
+++ rize-cockpit/src/main/java/rize/os/cockpit/security/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package rize.os.cockpit.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+import rize.os.security.oauth2.client.AuthenticatedUserOidcService;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration
+{
+    @Bean
+    JwtDecoder jwtDecoder(@Value("${spring.security.oauth2.client.provider.keycloak.issuer-uri}") String issuerUri)
+    {
+        return JwtDecoders.fromIssuerLocation(issuerUri);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   AuthenticatedUserOidcService authenticatedOidcUserService) throws Exception
+    {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().hasRole(CockpitRoles.PLATFORM_ADMIN))
+                .oauth2Login(oauth2Login ->
+                        oauth2Login.userInfoEndpoint(userInfo -> userInfo.oidcUserService(authenticatedOidcUserService)));
+
+        return http.build();
+    }
+}
