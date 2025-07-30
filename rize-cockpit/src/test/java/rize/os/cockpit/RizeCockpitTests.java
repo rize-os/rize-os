716bdeba76c14a3b63b35d5982bccfc5cebd3817
Dennis Schäfer <dennis.schaefer94@gmail.com>
2025-07-30 22:57:11 +0200

    Added DynamicPropertySource to add Keycloak Server URL to properties

--- rize-cockpit/src/test/java/rize/os/cockpit/RizeCockpitTests.java
+++ rize-cockpit/src/test/java/rize/os/cockpit/RizeCockpitTests.java
@@ -3,11 +3,19 @@ package rize.os.cockpit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
 @Import(TestcontainersConfiguration.class)
 class RizeCockpitTests
 {
+    @DynamicPropertySource
+    static void containerProperties(DynamicPropertyRegistry registry)
+    {
+        TestcontainersConfiguration.updateContainerProperties(registry);
+    }
+
     @Test
     void contextLoads()
     {
