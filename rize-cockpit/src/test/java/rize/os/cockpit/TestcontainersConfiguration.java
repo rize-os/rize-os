40558f3da7db016cae457e3f0524400d68d80dbd
Dennis Schäfer <dennis.schaefer94@gmail.com>
2025-07-30 20:03:02 +0200

    Added Testcontainer configuration of Keycloak for Cockpit

--- rize-cockpit/src/test/java/rize/os/cockpit/TestcontainersConfiguration.java
+++ rize-cockpit/src/test/java/rize/os/cockpit/TestcontainersConfiguration.java
@@ -1,8 +1,10 @@
 package rize.os.cockpit;
 
+import dasniko.testcontainers.keycloak.KeycloakContainer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -11,11 +13,25 @@ class TestcontainersConfiguration
 {
     private static final String POSTGRES_IMAGE = "postgres:17.5";
 
+    private static final String KEYCLOAK_IMAGE = "keycloak/keycloak:26.3.2";
+    private static final String KEYCLOAK_REALM_IMPORT_FILE = "keycloak/administration-realm.json";
+    private static final String KEYCLOAK_ADMIN_USERNAME = "admin";
+    private static final String KEYCLOAK_ADMIN_PASSWORD = "password";
+
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgresContainer()
     {
         return new PostgreSQLContainer<>(DockerImageName.parse(POSTGRES_IMAGE));
+    }
+
+    @Bean
+    GenericContainer<?> keycloakContainer()
+    {
+        return new KeycloakContainer(KEYCLOAK_IMAGE)
+                .withRealmImportFile(KEYCLOAK_REALM_IMPORT_FILE)
+                .withAdminUsername(KEYCLOAK_ADMIN_USERNAME)
+                .withAdminPassword(KEYCLOAK_ADMIN_PASSWORD);
     }
 
     /*
